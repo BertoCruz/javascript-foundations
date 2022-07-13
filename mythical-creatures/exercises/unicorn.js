b6f72abe7a29e97e822e1ebd1ebf868c7acbfef1
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-07-12 23:03:18 -0600

    Finished Unicorn testing

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,17 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
+  }
+
+  isWhite() {
+    //i'm proud of you for practicing best coding practice right here
+    //DRY method here we go!
+    return this.color === "white";
+  }
+
+  says(stuff) {
+    return `**;* ${stuff} *;**`
   }
 }
 
