103d97fe64684066d525f07bb1debb2b18b20cf2
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-07-12 23:04:06 -0600

    Finsihed Dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
 
+class Dragon {
+  constructor (name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.hungerCount = 0;
+  }
+
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.hungerCount++;
+    if(this.hungerCount === 3){
+    return this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon
