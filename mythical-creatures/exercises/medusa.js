42d359e9702ec213c9f9a4adc751c66012eacb6c
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-07-28 09:49:30 -0600

    Upload the completed medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,28 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(medusaName) {
+    this.name = medusaName;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victimPerson){
+    var newStatue = new Statue(victimPerson.name);
+    var relievedPerson = new Person();
+
+    if(this.statues.length < 3){
+      this.statues.push(newStatue);
+    } else if (this.statues.length === 3){
+      relievedPerson.name = this.statues[0].name;
+      relievedPerson.mood = 'relieved';
+      this.statues.shift();
+      this.statues.push(newStatue);
+      return relievedPerson;
+    }
+
+  }
+}
+
+
+module.exports = Medusa;
