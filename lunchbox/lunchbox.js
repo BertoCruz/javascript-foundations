bdba0b62eeaa1436205e2ba3d5c8a0c30c9111a3
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-08-05 18:21:32 -0600

    Add completed lunchbox exercises

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+    constructor(boxItem) {
+        this.owner = boxItem.owner;
+        this.material = boxItem.madeOf;
+        this.shape = boxItem.shape;
+        this.color = boxItem.color;
+        this.snacks = [];
+    }
 
+    acquireSnack(snack) {
+        snack.isInLunchBox = true;
+        this.snacks.push(snack);
+    }
+
+    findHealthySnacks(){
+        var healthySnacks = [];
+        for (var i = 0; i < this.snacks.length; i++) {
+            if (this.snacks[i].checkForHealthy()){
+                healthySnacks.push(this.snacks[i].type);
+            }
+        }
+
+        return healthySnacks;
+    }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
+    constructor(item){
+        this.deliciousLevel = "extra";
+        this.type = item;
+        this.amount = 100;
+        this.isInLunchBox = false; 
+        this.cuttingItClose = false;
+    }
 
+    getEaten() {
+        this.amount -= 10;
+        var currentPercent = (this.amount/100) * 100;
+
+        if (currentPercent <= 20) {
+            this.cuttingItClose = true;
+        }
+    }
+
+    checkForHealthy() {
+        var type = this.type.toLowerCase();
+        if (type.includes("fruit")) return true;
+        else return false;
+    }
 }
 
 module.exports = Snack;
